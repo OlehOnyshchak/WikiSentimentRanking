48fbb30d7e63625aa9901607bd7f75f8ba5db79b
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2019-07-04 20:07:39 +0300

    Implemented a quary based on what words page contains. Additional refactoring to the function

--- wiki_reader/reader.py
+++ wiki_reader/reader.py
@@ -11,21 +11,36 @@ def _clean(wiki_text):
 def _dump(path, data):
     with open(path, 'w', encoding='utf8') as outfile:  
         json.dump(data, outfile, indent=2, ensure_ascii=False)
+        
+def query_size(request):
+    site = pywikibot.Site()
+    pages = list(site.search(request, namespaces=[0]))
+    
+    return len(pages)
 
-def query(request, batch_size=100, limit=1000):
+def query(request, batch_size=100, limit=1000, is_category=False, debug_info=True):
     requests_base = Path('../requests')
     requests_path = requests_base / request
- 
+    
     if requests_path.exists():
-        return
+        if debug_info: print('Request has already been downloaded')
+        return requests_path
 
-    requests_path.mkdir(parents=True)
+    requests_path.mkdir(parents=True, exist_ok=True)
     
     site = pywikibot.Site()
-    category = pywikibot.Category(site, request)
-    pages = category.articles(namespaces=[0], #type of entities to query, 0 = page
+    
+    if is_category:
+        category = pywikibot.Category(site, request)
+        pages = list(category.articles(namespaces=[0], # type of entities to query, 0 = page
                               recurse=True, # also query all subpages
-                              content = True) # preloaod pages
+                              total=limit,
+                              content=True)) # preloaod pages
+    else:
+        pages = list(site.search(request,
+                                 total=limit,
+                                 content=True, # preloaod pages
+                                 namespaces=[0])) # type of entities to query, 0 = page
     
     count = 0
     pages_key = 'pages'
@@ -41,10 +56,10 @@ def query(request, batch_size=100, limit=1000):
         if count % batch_size == 0:
             _dump(requests_path / (str(count) + '.json'), data)
             data = { pages_key: [] }
-            print('Dumped {} pages'.format(count))
-        
-        if count == limit:
-            break
+            if debug_info: print('Dumped {} pages'.format(count))
             
     if len(data[pages_key]):
         _dump(requests_path / (str(count) + '.json'), data)
+        if debug_info: print('Dumped {} pages'.format(count))
+            
+    return requests_path
