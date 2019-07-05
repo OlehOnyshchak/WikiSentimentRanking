511303c537aa9913be5efb48d2499c5d0e57943c
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2019-07-05 23:07:00 +0300

    Implemented multithreded processing of requests

--- wiki_reader/reader.py
+++ wiki_reader/reader.py
@@ -18,28 +18,34 @@ def query_size(request):
     
     return len(pages)
 
-def query(request, batch_size=100, limit=1000, is_category=False, debug_info=True):
+def get_requests_path(request):
     requests_base = Path('../requests')
     requests_path = requests_base / request
     
-    if requests_path.exists():
+    is_exist = requests_path.exists()
+    if not is_exist:
+        requests_path.mkdir(parents=True)
+      
+    return (requests_path, is_exist)
+
+def query(request, batch_size=100, limit=1000, is_category=False, preload_content=True, debug_info=True):
+    requests_path, existed = get_requests_path(request)
+    
+    if existed:
         if debug_info: print('Request has already been downloaded')
         return requests_path
-
-    requests_path.mkdir(parents=True, exist_ok=True)
     
-    site = pywikibot.Site()
-    
+    site = pywikibot.Site()    
     if is_category:
         category = pywikibot.Category(site, request)
         pages = list(category.articles(namespaces=[0], # type of entities to query, 0 = page
                               recurse=True, # also query all subpages
                               total=limit,
-                              content=True)) # preloaod pages
+                              content=preload_content)) # preloaod pages
     else:
         pages = list(site.search(request,
                                  total=limit,
-                                 content=True, # preloaod pages
+                                 content=preload_content, # preloaod pages
                                  namespaces=[0])) # type of entities to query, 0 = page
     
     count = 0
@@ -55,10 +61,10 @@ def query(request, batch_size=100, limit=1000, is_category=False, debug_info=Tru
         if count % batch_size == 0:
             _dump(requests_path / (str(count) + '.json'), data)
             data = []
-            if debug_info: print('Dumped {} pages'.format(count))
+            if debug_info: print('Dumped {} pages | {}'.format(count, request))
             
     if len(data):
         _dump(requests_path / (str(count) + '.json'), data)
-        if debug_info: print('Dumped {} pages'.format(count))
+        if debug_info: print('Dumped {} pages | {}'.format(count, request))
             
     return requests_path
