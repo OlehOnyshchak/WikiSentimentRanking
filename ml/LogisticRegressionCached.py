8ed84d987691bed55b1499cbae0a92f4e02e0db2
Oleh Onyshchak <oleh.onyshchak@gmail.com>
2019-07-06 01:15:17 +0300

    Imported ml module from main pipeline

--- ml/LogisticRegressionCached.py
+++ ml/LogisticRegressionCached.py
@@ -1,9 +1,9 @@
 from pyspark.ml.classification import LogisticRegressionModel
 from pyspark.ml import PipelineModel
 
-def readFromCache():
-    lrModel = LogisticRegressionModel.load("./train/lr.model")
-    pipelineModel = PipelineModel.load('./train/pipeline.model')
+def readFromCache(train_path):
+    lrModel = LogisticRegressionModel.load(train_path + "lr.model")
+    pipelineModel = PipelineModel.load(train_path + 'pipeline.model')
     print('models loaded')
     
     return (lrModel, pipelineModel)
