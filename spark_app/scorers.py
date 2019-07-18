2521d5f96103d0bcd5b08659622337538277624c
OlehLuk <oleh.lukianykhin.a@gmail.com>
2019-07-18 13:26:39 +0300

    Started integration of advanced LR to spark_app scorer

--- spark_app/scorers.py
+++ spark_app/scorers.py
@@ -1,5 +1,29 @@
-def score_text(text):
+
+# method may be 'vader', 'lr' or 'lr-advanced'
+def score_text(text, method='vader', pipelineModel=None, lrModel=None):
+    if method == 'lr-advanced' and pipelineModel is not None and lrModel is not None:
+        return lr_advanced_scorer(text, lrModel, pipelineModel)
+    else:
+        return vader_scorer(text)
+    
+def vader_scorer(text):
     from nltk.sentiment.vader import SentimentIntensityAnalyzer
     sid = SentimentIntensityAnalyzer()
     score = sid.polarity_scores(text)
     return score["compound"]
+
+# TO DO: fix SparkSession varible `spark`
+def lr_scorer(text, lrModel, pipelineModel):
+    df = spark.createDataFrame([(text, 2)], ['text', 'target'])
+    df_transformed = pipelineModel.transform(df) # To fix
+    predictions = lrModel.transform(df_transformed)
+    predictions = predictions.select(['text', 'probability', 'prediction'])
+    pd_predictions = predictions.toPandas()
+    positive_probability = pd_predictions.iloc[0]['probability'][1]
+    overall_probability = 2 * positive_probability - 1   
+    return overall_probability
+
+def lr_advanced_scorer(text, lrModel, pipelineModel):
+    from nltk import tokenize
+    sentences = tokenize.sent_tokenize(text)
+    return np.mean([ lr_scorer(s, lrModel, pipelineModel) for s in sentences])
